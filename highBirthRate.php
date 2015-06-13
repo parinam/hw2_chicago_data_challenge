313a5a32cd8568842c9e85439fb97afe93dc88a1
parinam <parina.madaan@gmail.com>
2015-06-13 13:14:25 -0500

    Adding more methods for the correlation year 2000

--- highBirthRate.php
+++ highBirthRate.php
@@ -8,16 +8,16 @@
  */
 class highBirthRate
 {
-    var $result_total_birth = [];
-    var $result_low_birth_weight = [];
-    var $result_low_birth_weight1 = [];
-    var $result_total_birth1 = [];
+    var $result_total_birth1999 = [];
+    var $result_low_birth_weight1999 = [];
+    var $result_low_birth_weight2000 = [];
+    var $result_total_birth2000 = [];
 
     /***
-     * Method for the public health statistics file for births and birth rate
+     * Method for the public health statistics file for births and birth rate for the year 1999
      * @param $file
      */
-    public function parseBirthRateCsv($file)
+    public function parseBirthRate1999Csv($file)
     {
         $this->checkFilePath($file);
         $file_handle = fopen($file, "r");
@@ -25,9 +25,9 @@ class highBirthRate
             $line_of_text = fgetcsv($file_handle, 1024);
             /**This is a hack, because the id for chicago is 100 in the birth file and 0 in the low birth csv file */
             if ($line_of_text[1] === "Chicago") { //hack for chicago, since id is different
-                $result_total_birth[100] = $line_of_text[2];
+                $result_total_birth1999[100] = $line_of_text[2];
             } else {
-                $result_total_birth[$line_of_text[0]] = $line_of_text[2]; //area id = number of max births in year 1999
+                $result_total_birth1999[$line_of_text[0]] = $line_of_text[2]; //area id = number of max births in year 1999
             }
         }
         fclose($file_handle);
@@ -39,47 +39,19 @@ class highBirthRate
             file_exists($file);
         } catch (Exception $e) {
             echo "Error : $e->getMessage()\n";
-
+            exit(0);
         }
 
     }
 
-    public function parseLowBirthWeightCsv($file)
+    public function parseLowBirthWeight1999Csv($file)
     {
         $this->checkFilePath($file);
         $file_handle = fopen($file, "r");
         while (!feof($file_handle)) {
             $line_of_text = fgetcsv($file_handle, 1024);
-            $result_low_birth_weight[$line_of_text[0]] = $line_of_text[2]; //area id = number of low births in 1999
+            $result_low_birth_weight1999[$line_of_text[0]] = $line_of_text[2]; //area id = number of low births in 1999
             $community_area[$line_of_text[0]] = $line_of_text[1]; //area id = Neighbourhood community name
-        }
-        fclose($file_handle);
-    }
-
-    public function parseLowBirthRate1csv($file)
-    {
-        $this->checkFilePath($file);
-        $file_handle = fopen($file, "r");
-        while (!feof($file_handle)) {
-            $line_of_text = fgetcsv($file_handle, 1024);
-            $result_low_birth_weight1[$line_of_text[0]] = $line_of_text[3]; //area id = number of low births in 2000
-            $community_area[$line_of_text[0]] = $line_of_text[1]; //area id = neighbourhood community name
-        }
-        fclose($file_handle);
-    }
-
-    public function parseBirthRate1Csv($file)
-    {
-        $this->checkFilePath($file);
-        $file_handle = fopen($file, "r");
-        while (!feof($file_handle)) {
-            $line_of_text = fgetcsv($file_handle, 1024);
-            /**This is a hack, because the id for chicago is 100 in the birth file and 0 in the low birth csv file */
-            if ($line_of_text[1] === "Chicago") { //hack for chicago, since id is different
-                $result_total_birth1[100] = $line_of_text[3];
-            } else {
-                $result_total_birth1[$line_of_text[0]] = $line_of_text[3]; //area id = number of max births in year 2000
-            }
         }
         fclose($file_handle);
     }
@@ -87,15 +59,61 @@ class highBirthRate
     /**Calculate the high birth weight rate for the year 1999
      * High birth weight rate = Total birth rate - low birth weight rate
      */
-    public function parseHighBirth($file)
+    public function parseHighBirth1999($file)
     {
         $this->checkFilePath($file);
         $file_handle = fopen($file,"r");
         while (!feof($file_handle)){
-        foreach ($result_low_birth_weight as $area_id => $low_birth_weight) {
-            $high = ($result_birth_rate[$area_id] - $result_low_birth_weight[$area_id]);
+        foreach ($result_low_birth_weight1999 as $area_id => $low_birth_weight) {
+            $high1999 = ($result_birth_rate1999[$area_id] - $result_low_birth_weight1999[$area_id]);
 
         }}
 
     }
+    /***
+     * Method for the public health statistics file for births and birth rate for the year 1999
+     * @param $file
+     */
+
+
+    public function parseLowBirthRate2000csv($file)
+    {
+        $this->checkFilePath($file);
+        $file_handle = fopen($file, "r");
+        while (!feof($file_handle)) {
+            $line_of_text = fgetcsv($file_handle, 1024);
+            $result_low_birth_weight2000[$line_of_text[0]] = $line_of_text[3]; //area id = number of low births in 2000
+            $community_area[$line_of_text[0]] = $line_of_text[1]; //area id = neighbourhood community name
+        }
+        fclose($file_handle);
+    }
+
+    public function parseBirthRate2000Csv($file)
+    {
+        $this->checkFilePath($file);
+        $file_handle = fopen($file, "r");
+        while (!feof($file_handle)) {
+            $line_of_text = fgetcsv($file_handle, 1024);
+            /**This is a hack, because the id for chicago is 100 in the birth file and 0 in the low birth csv file */
+            if ($line_of_text[1] === "Chicago") { //hack for chicago, since id is different
+                $result_total_birth2000[100] = $line_of_text[3];
+            } else {
+                $result_total_birth2000[$line_of_text[0]] = $line_of_text[3]; //area id = number of max births in year 2000
+            }
+        }
+        fclose($file_handle);
+    }
+
+    public function parseHighBirth2000($file)
+    {
+        $this->checkFilePath($file);
+        $file_handle = fopen($file,"r");
+        while (!feof($file_handle)){
+            foreach ($result_low_birth_weight2000 as $area_id => $low_birth_weight) {
+                $high2000 = ($result_birth_rate2000[$area_id] - $result_low_birth_weight2000[$area_id]);
+
+            }}
+
+    }
+
 }
